e2dcc18084e6b403e83cc7ad06b43297900cff2f
Sachith Rukshan <sachithrukshanmail@gmail.com>
2018-08-13 02:17:53 +0530

    MainSelector Created

---
+++ app/src/main/java/com/example/user/mymedic/MainSelector.java
@@ -0,0 +1,13 @@
+package com.example.user.mymedic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainSelector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_selector);
+    }
+}
