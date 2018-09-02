ba5f83d423c05196b8461afc27fdbb51095a36b8
Slash De Silva <slashde97@gmail.com>
2018-09-02 17:37:29 +0530

    test

--- app/src/main/java/com/example/user/mymedic/MainActivity.java
+++ app/src/main/java/com/example/user/mymedic/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.user.mymedic;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class MainSelector extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_selector);
+        setContentView(R.layout.homescreen);
+
     }
+
 }
+
+
